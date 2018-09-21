5181ee1413e69c80eb2be0489ef76971c9dba954
murd0ck@ukr.net <diuryn@intropro.com>
2018-09-21 20:40:03 +0300

    [OBSERVER: v1.0.0]: Server refactoring

--- server/index.js
+++ server/index.js
@@ -1,37 +1,13 @@
-//noinspection JSUnresolvedVariable
-const fs = require('fs');
-// const http = require('http');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
 const cors = require('cors');
-// environmental variable importing
-const dotenv = require('dotenv');
-// Webpack imports
 const webpack = require('webpack');
 const config = require('../webpack.config');
-let useragent = require('useragent');
-dotenv.config();
-
-const PATH_DATA = path.resolve(__dirname, './data');
-
 const compiler = webpack(config);
 const serverPort = 8086;
-const env = process.env;
 const app = express();
-
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: env.WS_PORT });
-
-const broadcast = (data, ws) => {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN && client !== ws) {
-            client.send(JSON.stringify(data))
-        }
-    })
-};
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(require("webpack-dev-middleware")(compiler, {
@@ -48,7 +24,6 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// ========= WebSocket ==================
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -57,12 +32,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-// app root folder path
-const root = path.resolve(__dirname, '..'); // __dirname is a global variable available in any file and refers to that file's directory path
-//static folders
-app.use('/public',express.static(path.join(root, 'public')));
 
-//app.use('/data', express.static(PATH_DATA));
+const root = path.resolve(__dirname, '..');
+app.use('/public',express.static(path.join(root, 'public')));
 
 // Handle route errors
 app.use((err, req, res, next) => {
@@ -72,50 +44,16 @@ app.use((err, req, res, next) => {
 });
 
 app.get('*', (req, res, next) => {
-    // console.log("[SERVER]: SEND FILE => ", req.url);
     res.sendFile('/index.html', {
         root: path.join(root, 'public')
     });
 });
 
-// // catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use((req, res, next) => {
-    console.log("404 ? ", req.body);
     const err = new Error('Not Found');
     err.status = 404;
     return next(err);
-});
-
-wss.on('connection', (ws) => {
-
-    ws.on('message', (message) => {
-        const LOG_URI = path.resolve(__dirname + "/log/", "logWS.json");
-        const data = message;
-        console.log("%received: ", data);
-        try {
-            if (JSON.parse(data).sensor) {
-                ws.send(JSON.stringify({
-                    data: data
-                }));
-
-                broadcast({
-                    data: data
-                }, ws);
-
-                fs.appendFileSync(LOG_URI, data);
-            }
-        } catch (e) {
-            console.log("Error on parse JSON: ", e);
-        }
-    });
-
-    ws.on('close', () => {
-
-        broadcast({
-            type: 'CLOSED',
-            reason: 'WS closed connection'
-        }, ws)
-    })
 });
 
 app.listen(serverPort, (err, res) => {
